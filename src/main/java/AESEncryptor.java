ede53f2986f64816bc97450dc8856d075a14aa93
Joseph Cotton <jtcotton63@gmail.com>
2016-01-13 13:22:58 -0700

    Removed variables as static global variables 2

--- src/main/java/AESEncryptor.java
+++ src/main/java/AESEncryptor.java
@@ -3,43 +3,39 @@
  */
 public class AESEncryptor {
 
-    static Word[] keySchedule = null;
     static int keyScheduleIdx = 0;
-    static State state = null;
-    static int nK = -1;
-    static int nR = -1;
-    static int ROUNDS = -1;
 
     public static String cipher(String data, String keyAsString) {
 
         Word[] key = WordHelper.toWordArray(keyAsString);
-        nK = key.length;
-        nR = detNR();
-        ROUNDS = nR+1;
-        prepState(data);
-        keySchedule = KeyExpander.expandCypherKey(nK, ROUNDS, key);
+        int nK = key.length;
+        int nR = detNR(nK);
+        int ROUNDS = nR+1;
+        State state = prepState(data);
+        Word[] keySchedule = KeyExpander.expandCypherKey(nK, ROUNDS, key);
+        keyScheduleIdx = 0;
 
         // Input round
-        addRoundKey();
+        addRoundKey(state, keySchedule);
 
         // nR - 1 rounds
         for(int round = 1; round < ROUNDS - 1; round++) {
-            subBytes();
-            shiftRows();
+            subBytes(state, nK);
+            shiftRows(state);
             state = MixColumnsHelper.mixColumns(state);
-            addRoundKey();
+            addRoundKey(state, keySchedule);
         }
 
         // Last rounds
-        subBytes();
-        shiftRows();
-        addRoundKey();
+        subBytes(state, nK);
+        shiftRows(state);
+        addRoundKey(state, keySchedule);
 
         return state.toString();
 
     }
 
-    private static int detNR() {
+    private static int detNR(int nK) {
         if(nK == 4)
             return 10;
         if(nK == 6)
@@ -51,8 +47,8 @@ public class AESEncryptor {
     }
 
     // This is a duplicate function; should be combined with WordHelper.toWordArray
-    private static void prepState(String data) {
-        state = new State();
+    private static State prepState(String data) {
+        State state = new State();
         int dataIdx = 0;
         for(int x = 0; x < 4; x++) {
             for (int y = 0; y < 8; y++) {
@@ -61,29 +57,30 @@ public class AESEncryptor {
                 dataIdx++;
             }
         }
+        return state;
     }
 
-    private static void subBytes() {
+    private static void subBytes(State state, int nK) {
         for(int i = 0; i < nK; i++) {
             state.setColumn(i, SubWordHelper.subWord(state.getColumn(i)));
         }
     }
 
-    private static void shiftRows() {
+    private static void shiftRows(State state) {
         // once
-        shiftRows(2);
+        shiftRows(state, 2);
 
         // twice
-        shiftRows(4);
-        shiftRows(4);
+        shiftRows(state, 4);
+        shiftRows(state, 4);
 
         // thrice
-        shiftRows(6);
-        shiftRows(6);
-        shiftRows(6);
+        shiftRows(state, 6);
+        shiftRows(state, 6);
+        shiftRows(state, 6);
     }
 
-    private static void shiftRows(int startRow) {
+    private static void shiftRows(State state, int startRow) {
         int tempNibble1 = state.getColumn(0).getNibbleAt(startRow);
         int tempNibble2 = state.getColumn(0).getNibbleAt(startRow+1);
         state.getColumn(0).setNibbleAt(startRow, state.getColumn(1).getNibbleAt(startRow));
@@ -96,7 +93,7 @@ public class AESEncryptor {
         state.getColumn(3).setNibbleAt(startRow+1, tempNibble2);
     }
 
-    private static void addRoundKey() {
+    private static void addRoundKey(State state, Word[] keySchedule) {
         for(int i = 0; i < 4; i++) {
             state.setColumn(i, WordHelper.xor(state.getColumn(i), keySchedule[keyScheduleIdx]));
             keyScheduleIdx++;
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,12 +3,25 @@
  */
 public class Main {
 
-    private static String data = "3243f6a8885a308d313198a2e0370734";
-    private static String KEY_128 = "2b7e151628aed2a6abf7158809cf4f3c";
-
     public static void main(String[] args) {
-        String result = AESEncryptor.cipher(data, KEY_128);
+        // Set 1
+        String data1 = "3243f6a8885a308d313198a2e0370734";
+        String KEY_128_1 = "2b7e151628aed2a6abf7158809cf4f3c";
+        String expectedResult1 = "3925841d 02dc09fb dc118597 196a0b32";
+        run(data1, KEY_128_1, expectedResult1);
+
+        String data2 = "00112233445566778899aabbccddeeff";
+        String KEY_128_2 = "000102030405060708090a0b0c0d0e0f";
+        String expectedResult2 = "69c4e0d8 6a7b0430 d8cdb780 70b4c55a";
+        run(data2, KEY_128_2, expectedResult2);
+    }
+
+    private static void run(String data, String key, String expectedResult) {
+        String result = AESEncryptor.cipher(data, key);
         System.out.println(result);
+        if(!result.equals(expectedResult)) {
+            throw new RuntimeException("Result differend from expected result");
+        }
     }
 
 }
